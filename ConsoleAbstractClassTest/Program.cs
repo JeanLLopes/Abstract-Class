334a5986eb79ce544b1805bb75d1584306e18a9d
jlopes <jean.llopes@outlook.com>
2018-08-27 00:18:08 -0300

    Add project files.

---
+++ ConsoleAbstractClassTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAbstractClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
